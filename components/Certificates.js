45917c7bf714f7a0befec1c3f589fead2813bd9b
basat <itzbasatmaqsood@gmail.com>
2025-01-17 13:06:29 +0500

    Certificates added

--- components/Certificates.js
+++ components/Certificates.js
@@ -2,22 +2,70 @@
  const Certificates = () => {
   const certificates = [
     {
+      id: 6,
+      title: "META Certified Frontend Developer",
+      issuer: "META through Coursera",
+      link: "https://www.credly.com/badges/be7b061c-21e1-4ca5-9e33-0380e0749463/public_url",
+    },
+    {
+      id: 13,
+      title: "Developing Back-End Apps with Node.js and Express",
+      issuer: "IBM through Coursera",
+      link: "https://coursera.org/share/1e99f1682f439af6a148d102db445dce",
+    },
+    {
       id: 1,
       title: "Developing Frontend Apps with React",
       issuer: "IBM through Coursera",
       link: "https://www.coursera.org/account/accomplishments/verify/0DBWZ9VJTGLF?utm_source=link&utm_medium=certificate&utm_content=cert_image&utm_campaign=sharing_cta&utm_product=course",
     },
     {
-      id: 2,
-      title: "Introduction to Front-End Development",
+      id: 11,
+      title: "Front-End Developer Capstone",
       issuer: "META through Coursera",
-      link: "https://coursera.org/share/dc9d90055feb8dfaccefa3f554ec9921",
+      link: "https://coursera.org/share/ab7fb4b1c9cbf3a44bf8e4605dc922b8",
+    },
+    {
+      id: 14,
+      title: "Introduction to Next.js",
+      issuer: "Coursera Instructor Network",
+      link: "https://coursera.org/share/de7b8394304aff50ee05d0a7b8c6f2c9",
+    },
+    {
+      id: 8,
+      title: "React Basics",
+      issuer: "META through Coursera",
+      link: "https://coursera.org/share/d096398c1db93a2bec143f9bddce525f",
+    },
+    {
+      id: 9,
+      title: "Advanced React",
+      issuer: "META through Coursera",
+      link: "https://coursera.org/share/aa30feb40eb84f415e88275d2afe55d4",
+    },
+    {
+      id: 10,
+      title: "Principles of UX/UI Design",
+      issuer: "META through Coursera",
+      link: "https://coursera.org/share/4cbe6c64497796baa47e54207c91bbd1",
     },
     {
       id: 3,
       title: "Programming with Javascript",
       issuer: "META through Coursera",
       link: "https://coursera.org/share/77b10cdcc04a781558da0b0bc69d3bad",
+    },
+    {
+      id: 7,
+      title: "HTML and CSS in depth",
+      issuer: "META through Coursera",
+      link: "https://coursera.org/share/6970f8ab75bf90fd51ac06308c86ba5e",
+    },
+    {
+      id: 2,
+      title: "Introduction to Front-End Development",
+      issuer: "META through Coursera",
+      link: "https://coursera.org/share/dc9d90055feb8dfaccefa3f554ec9921",
     },
     {
       id: 4,
@@ -30,6 +78,12 @@
       title: "Manage Your Versions with Git",
       issuer: "Coursera Project Network",
       link: "https://coursera.org/share/434b52fdcb676a6f4fc5fddd71024d5a",
+    },
+    {
+      id: 12,
+      title: "Coding Interview Preparation",
+      issuer: "META through Coursera",
+      link: "https://coursera.org/share/cc7de8e5c5c5c20397261e571d7ff3d8",
     },
   ];
 
