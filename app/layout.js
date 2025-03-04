38d43e762b210f6ad137f6a16cd63be1e23cf103
basat <itzbasatmaqsood@gmail.com>
2025-03-04 10:06:52 +0500

    Sitemap added

--- app/layout.js
+++ app/layout.js
@@ -17,7 +17,8 @@ const robotoMono = Roboto_Mono({
 });
 export const metadata = {
   title: "Basat Maqsood",
-  description: "Software Enginner and Web Developer at UET Taxila.",
+  description:
+    "Software Engineer | Full Stack Web Developer | Expertise in MERN Stack. Passionate about building Responsive Web applications and solving complex business problems",
 };
 
 export default function RootLayout({ children }) {
